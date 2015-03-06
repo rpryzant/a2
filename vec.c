806e2475aac38eb0155f2bba77b40148489b6752
rpryzant93 <reid.pryzant@gmail.com>
2015-03-05 21:02:09 -0500

    finished vector implementation

--- vec.c
+++ vec.c
@@ -1,5 +1,5 @@
 /*
- * Here's a vector implementation, hopefully.
+ * Here's a vector implementation
  */
 
 #include <stdio.h>
@@ -7,6 +7,61 @@
 
 #include "vec.h"
 
-int main () {
+
+/*
+ * adds an element to the vector
+ */
+void v_add(vec v, void *new) {
+  if (v->size == v->limit) {
+    v->limit *= 2;
+    v->data = realloc(v->data, sizeof(void *) * v->limit);
+  }
+  v->data[v->size] = new;
+  v->size++;
+}
+
+/*
+ * retrieves an item from a vector
+ */
+void *v_get(vec v, int i) {
+  if (i >= v->limit) 
+    return;
+
+   return v->data[i];
+}
+
+/*
+ * sets the element at index i of vector v
+ */
+void v_set(vec v, int i, void *new) {
+  if (i >= v->limit)
+    return;
+
+  v->data[i] = new;
+}
+
+/*
+ * initializes a new vector and returns a pointer to it
+ */
+vec v_alloc(void) {
+  vec v = (vec)calloc(sizeof(vector *), 1);
+  v->size = 0;
+  v->limit = 10;
+  v->data = calloc(sizeof(void *) * v->limit, 1);
+  return v;
+}
+
+/*
+ * frees an existing vector
+ */
+void v_free(vec v) {
+  if (v == 0) 
+    return;
+
+  free(v->data);
+  free(v);
+}
+
+int main() {
   return 0;
 }
